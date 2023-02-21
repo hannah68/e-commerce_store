da92d78165ac871c5097db3e9bd2e48de0a558b6
hannah68 <reihaneh.naderi1989@gmail.com>
2023-02-21 11:06:18 +0000

    create home component layouts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// 1 item per row(1 column) has height of 400, 3 item per row has height of 335
+const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3: 335, 4: 350}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   category: string | undefined;
-  cols = 3
+  cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
 
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onCategoryName(category: string):void{
