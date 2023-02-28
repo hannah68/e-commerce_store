3b5e7a00ece170b6fac5dbc0b3fa3c24e7081099
hannah68 <reihaneh.naderi1989@gmail.com>
2023-02-28 08:27:45 +0000

    fetch products from api

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Product } from 'src/app/model/product.model';
 import { CartService } from 'src/app/services/cart.service';
+import { StoreService } from 'src/app/services/store.service';
 // 1 item per row(1 column) has height of 400, 3 item per row has height of 335
 const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3: 335, 4: 350}
 
@@ -8,12 +10,26 @@ const ROWS_HEIGHT: {[id:number]: number} = {1: 400, 3: 335, 4: 350}
   selector: 'app-home',
   templateUrl:'./home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy{
   category: string | undefined;
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols];
+  products: Product[] | undefined;
+  sort = 'desc';
+  limit = '12';
+  productsSubscription: Subscription | undefined;
 
-  constructor(private cartService: CartService){}
+  constructor(private cartService: CartService, private storeService: StoreService){}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(): void{
+    this.productsSubscription = this.storeService.getAllProducts(this.limit, this.sort).subscribe(_products => {
+      this.products = _products;
+    });
+  }
 
   onColumnsCountChange(colsNum: number): void{
     this.cols = colsNum;
@@ -32,5 +48,9 @@ export class HomeComponent {
       quantity: 1,
       id: product.id,
     });
+  }
+
+  ngOnDestroy(): void {
+    this.productsSubscription?.unsubscribe();
   }
 }
